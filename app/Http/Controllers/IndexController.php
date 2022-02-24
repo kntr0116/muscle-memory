e0b2f820e5595286dca4c8a45c76c0603674fe36
kntr0116 <kntr0116@gmail.com>
2022-02-24 23:02:12 +0900

    ログイン時の名前表示機能を作成中

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function index(Request $request)
     {
+        $user = Auth::user();
         $items = DB::select('select * from posts');
         return view('main.index', ['items' => $items]);
     }
